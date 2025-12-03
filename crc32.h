f5d0a45647e53613629f6d0c9ae02a05fbf1c3d5
Alessandro Piras <laynor@gmail.com>
2025-12-03 19:36:24 +0900

    Add missing #include directives to crc32.h

--- crc32.h
+++ crc32.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stdlib.h>
 void crc32_init_table(void);
 uint32_t crc32_calc_raw(uint32_t crc, const uint32_t *data, size_t word_count);
 uint32_t crc32_calc(const void *data, size_t len);
